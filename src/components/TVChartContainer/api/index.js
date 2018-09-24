42d7678471ca0577bf88037d5176f3f9754eb651
bitxmen <bitxmenit@gmail.com>
2018-09-24 10:02:01 +0700

    len

---
+++ src/components/TVChartContainer/api/index.js
@@ -0,0 +1,156 @@
+import historyProvider from './historyProvider'
+
+//  const supportedResolutions = ["1", "3", "5", "15", "30", "60", "120", "240", "D"]
+const supportedResolutions = ["D"]
+
+const config = {
+	supported_resolutions: supportedResolutions
+};
+
+export default {
+	onReady: cb => {
+		// console.log('=====onReady running')	
+		setTimeout(() => cb(config), 0)
+
+	},
+	searchSymbols: (userInput, exchange, symbolType, onResultReadyCallback) => {
+		// console.log('====Search Symbols running')
+
+		var crytocoin = {
+			btc: {
+				symbol: "BTC",
+				full_name: "www.bitxmen.com",
+				description: "Bitcoin",
+				exchange: "BITXMEN.COM",
+				ticker: "BTC",
+				type: "Crypto",
+			},
+			eth: {
+				symbol: "ETH",
+				full_name: "www.bitxmen.com",
+				description: "Ethereum",
+				exchange: "BITXMEN.COM",
+				ticker: "ETH",
+				type: "Crypto",
+			},
+			bch: {
+				symbol: "BCH",
+				full_name: "www.bitxmen.com",
+				description: "Bitcoin Cash",
+				exchange: "BITXMEN.COM",
+				ticker: "BCH",
+				type: "Crypto",
+			},
+			doge: {
+				symbol: "DOGE",
+				full_name: "www.bitxmen.com",
+				description: "Dogecoin",
+				exchange: "BITXMEN.COM",
+				ticker: "DOGE",
+				type: "Crypto",
+			},
+			ltc: {
+				symbol: "LTC",
+				full_name: "www.bitxmen.com",
+				description: "Litecoin",
+				exchange: "BITXMEN.COM",
+				ticker: "LTC",
+				type: "Crypto",
+			},
+			trx: {
+				symbol: "TRX",
+				full_name: "www.bitxmen.com",
+				description: "Tron",
+				exchange: "BITXMEN.COM",
+				ticker: "TRX",
+				type: "Crypto",
+			},
+			usdt: {
+				symbol: "USDT",
+				full_name: "www.bitxmen.com",
+				description: "Tether",
+				exchange: "BITXMEN.COM",
+				ticker: "USDT",
+				type: "Crypto",
+			},
+		}
+		onResultReadyCallback([
+			crytocoin.btc,
+			crytocoin.eth,
+			crytocoin.doge,
+			crytocoin.usdt,
+			crytocoin.bch,
+			crytocoin.ltc,
+			crytocoin.trx
+		])
+	},
+	resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
+
+		// console.log({split_data})
+		var symbol_stub = {
+			name: symbolName,
+			description: '',
+			type: 'crypto',
+			session: '24x7',
+			timezone: "Asia/Bangkok",//'Etc/UTC',
+			ticker: symbolName,
+			exchange: "BITXMEN",
+			minmov: 1,
+			pricescale: 1,
+			has_intraday: true,
+			intraday_multipliers: ['50', '100'],
+			supported_resolution: supportedResolutions,
+			volume_precision: 8,
+			data_status: 'streaming',
+		}
+		setTimeout(function () {
+			onSymbolResolvedCallback(symbol_stub)
+			// console.log('Resolving that symbol....', symbol_stub)
+		}, 0)
+
+
+		// onResolveErrorCallback('Not feeling it today')
+
+	},
+	getBars: function (symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
+		// console.log('=====getBars running', symbolInfo)
+		// console.log('function args',arguments)
+		// console.log(`Requesting bars between ${new Date(from * 1000).toISOString()} and ${new Date(to * 1000).toISOString()}`)
+		historyProvider.getBars(symbolInfo, resolution, from, to, firstDataRequest)
+			.then(bars => {
+				if (bars.length) {
+					onHistoryCallback(bars, { noData: false })
+				} else {
+					onHistoryCallback(bars, { noData: true })
+				}
+			}).catch(err => {
+				console.log({ err })
+				onErrorCallback(err)
+			})
+
+	},
+	subscribeBars: (symbolInfo, resolution, onRealtimeCallback, subscribeUID, onResetCacheNeededCallback) => {
+		// console.log('=====subscribeBars runnning')
+	},
+	unsubscribeBars: subscriberUID => {
+		// console.log('=====unsubscribeBars running')
+	},
+	calculateHistoryDepth: (resolution, resolutionBack, intervalBack) => {
+		//optional
+		// console.log('=====calculateHistoryDepth running')
+		// while optional, this makes sure we request 24 hours of minute data at a time
+		// CryptoCompare's minute data endpoint will throw an error if we request data beyond 7 days in the past, and return no data
+		return resolution < 60 ? { resolutionBack: 'D', intervalBack: '1' } : undefined
+	},
+	getMarks: (symbolInfo, startDate, endDate, onDataCallback, resolution) => {
+		//optional
+		console.log('=====getMarks running')
+	},
+	getTimeScaleMarks: (symbolInfo, startDate, endDate, onDataCallback, resolution) => {
+		//optional
+		console.log('=====getTimeScaleMarks running')
+	},
+	getServerTime: cb => {
+		// console.log('=====getServerTime running')
+	}
+}
---
+++ src/components/TVChartContainer/index.jsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import './index.css';
+import Datafeed from './api/'
+
+
+function getLanguageFromURL() {
+	const regex = new RegExp('[\\?&]lang=([^&#]*)');
+	const results = regex.exec(window.location.search);
+	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+export class TVChartContainer extends React.PureComponent {
+
+	static defaultProps = {
+		// symbol: 'Coinbase:BTC/USD',
+		// symbol: 'Bitxmen:BTC/VND',
+		symbol: (window.location.href.split("?")[1] == undefined ? "BTC" : window.location.href.split("?")[1]),
+		interval: 'D', // thời gian giữa 2 nến
+		containerId: 'tv_chart_container',
+		libraryPath: '/charting_library/',
+		chartsStorageUrl: 'https://saveload.tradingview.com',
+		chartsStorageApiVersion: '1.1',
+		clientId: 'tradingview.com',
+		userId: 'public_user_id',
+		fullscreen: true,
+		autosize: true,
+		studiesOverrides: {},
+	};
+
+	componentDidMount() {
+		const widgetOptions = {
+			debug: false,
+			symbol: this.props.symbol,
+			datafeed: Datafeed,
+			interval: this.props.interval,
+			container_id: this.props.containerId,
+			library_path: this.props.libraryPath,
+			locale: getLanguageFromURL() || 'vi',
+			disabled_features: ['use_localstorage_for_settings'],
+			enabled_features: ['study_templates'],
+			charts_storage_url: this.props.chartsStorageUrl,
+			charts_storage_api_version: this.props.chartsStorageApiVersion,
+			client_id: this.props.clientId,
+			user_id: this.props.userId,
+			fullscreen: this.props.fullscreen,
+			autosize: this.props.autosize,
+			studies_overrides: this.props.studiesOverrides,
+			timezone: "Asia/Bangkok",
+
+			numeric_formatting: { decimal_sign: "." },
+			customFormatters: {
+				// timeFormatter: {
+				// 	format: function (date) { var _format_str = '%h:%m'; return _format_str.replace('%h', date.getUTCHours(), 2).replace('%m', date.getUTCMinutes(), 2).replace('%s', date.getUTCSeconds(), 2); }
+				// },
+				dateFormatter: {
+					format: function (date) { 
+						let month = date.getUTCMonth() + 1 < 10 ? '0' + parseInt(date.getUTCMonth() + 1) : date.getUTCMonth() + 1
+						let day = date.getUTCDate() < 10 ? '0' + date.getUTCDate() : date.getUTCDate()
+						return day + '-' + month + '-' + date.getUTCFullYear() 
+					}
+				}
+			},
+			time_frames: [
+				// { text: "50y", resolution: "6M", description: "50 Years" },
+				// { text: "3y", resolution: "W", description: "3 Years", title: "3yr" },
+				// { text: "8m", resolution: "D", description: "8 Month" },
+				// { text: "3d", resolution: "5", description: "3 Days" },
+				// { text: "1000y", resolution: "W", description: "All", title: "All" },
+			],
+			//loading_screen: { backgroundColor: "#000000" },
+			overrides: {
+				// "paneProperties.crossHairProperties.width": "1",
+				"volumePaneSize": "large",
+				// "mainSeriesProperties.showCountdown": true,
+				// "paneProperties.rightAxisProperties.percentage":false,
+				// "paneProperties.background": "#131722",
+				// "paneProperties.vertGridProperties.color": "#363c4e",
+				// "paneProperties.horzGridProperties.color": "#363c4e",
+				"paneProperties.legendProperties.showSeriesOHLC": true,
+				
+				// "paneProperties.legendProperties.showSeriesTitles": false,
+				// "symbolWatermarkProperties.transparency": 90,
+				// "symbolWatermarkProperties.color" : "#944",
+				
+				// "scalesProperties.textColor": "#AAA",
+				// "mainSeriesProperties.candleStyle.wickUpColor": '#336854',
+				// "mainSeriesProperties.candleStyle.wickDownColor": '#7f323f',
+				// "mainSeriesProperties.style": 1,
+
+				"scalesProperties.lineColor" : "#E1ECF2",
+
+
+			}
+		};
+
+		window.TradingView.onready(() => {
+			const widget = window.tvWidget = new window.TradingView.widget(widgetOptions);
+
+			widget.onChartReady(() => {
+				console.log('Chart has loaded!')
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div
+				id={this.props.containerId}
+				className={'TVChartContainer'}
+			/>
+		);
+	}
+}
